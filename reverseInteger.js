4ced3ffb3d1bb37c45633fbb2da517fceee79b88
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2025-02-10 23:07:32 +0530

    update

---
+++ reverseInteger.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let res = 0;
+    if (x<0){
+        res = parseInt(String(x).slice('').split('').reverse().join('')) * -1;
+    }else {
+        res = parseInt(String(x).split('').reverse().join(''));
+    }
+    if(res > Math.pow(2, 31) -1 || res < -Math.pow(2,31)){
+        return 0
+    }
+    return res
+};
