8da25ba9661dc21cb56dc0e2af537a78d21ba853
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2025-02-11 10:19:47 +0530

    ,

---
+++ chunkarray.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    const chunkArray = [];
+
+    for (let i = 0; i < arr.length; i, i += size) {
+        chunkArray.push(arr.slice(i, i + size));
+    }
+
+    return chunkArray;
+};
