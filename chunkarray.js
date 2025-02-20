3e92afffc0bbd1bc4fc5cf14232abd24f3840045
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2025-02-20 10:33:20 +0530

    updated

--- chunkarray.js
+++ chunkarray.js
@@ -1,14 +1,20 @@
-/**
- * @param {Array} arr
- * @param {number} size
- * @return {Array}
- */
-var chunk = function(arr, size) {
-    const chunkArray = [];
+// /**
+//  * @param {Array} arr
+//  * @param {number} size
+//  * @return {Array}
+//  */
+// var chunk = function(arr, size) {
+//     const chunkArray = [];
 
-    for (let i = 0; i < arr.length; i, i += size) {
-        chunkArray.push(arr.slice(i, i + size));
-    }
+//     for (let i = 0; i < arr.length; i, i += size) {
+//         chunkArray.push(arr.slice(i, i + size));
+//     }
 
-    return chunkArray;
-};
+//     return chunkArray;
+// };
+
+const add = (num) => num + 5;
+const multiple = (num) => num * 2;
+
+const result = multiple(add(3))
+console.log(result);
---
+++ closure.js
@@ -0,0 +1,12 @@
+// function outerFunction(){
+//     let a = 10
+//    return function innerFunction(){
+//         console.log(a+=1);
+//     }
+    
+// }
+
+// let result = outerFunction();
+// result()
+// result()
+
